3ce300f223a994ff1067b3f259e6f740b730a6e1
smkim <kiak357@gmail.com>
2024-08-22 16:44:36 +0900

    OAuth 추가.  - gpts 에서 외부 로그인 가능하도록 api추가.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from typing import Any, Dict
+from fastapi import Body, FastAPI, Form, Request
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel, Field
 
 app = FastAPI(
@@ -6,6 +9,7 @@ app = FastAPI(
     description="Get a real quote said by SAM himself.",
 )
 
+user_token_db = {"ABCDEF": "SAM"}
 
 class Quote(BaseModel):
     quote: str = Field(
@@ -23,8 +27,34 @@ class Quote(BaseModel):
     response_description="A Quote object that contains the quote said by SAM and the date when the quote was said.",
     response_model=Quote,
 )
-def get_quote():
+def get_quote(request: Request):
+    print(request.headers)
     return {
         "quote": "Life is short so eat it all.",
         "year": 1950,
+    }
+
+
+@app.get(
+    "/authorize",
+    response_class=HTMLResponse,
+)
+def handle_authorize(client_id: str, redirect_uri: str, state: str):
+    return f"""
+    <html>
+        <head>
+            <title>SAM Log In</title>
+        </head>
+        <body>
+            <h1>Log Into SAM</h1>
+            <a href="{redirect_uri}?code=ABCDEF&state={state}">Authorize SAM GPT</a>
+        </body>
+    </html>
+    """
+
+
+@app.post("/token")
+def handle_token(code=Form(...)):
+    return {
+        "access_token": user_token_db[code]
     }
