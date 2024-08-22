65c924756fe306802baec3b27a5ea09c080980d9
smkim <kiak357@gmail.com>
2024-08-22 12:16:04 +0900

    update FastApi Quote api
    - 기본적인 sample api 개발.

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI(
-    title=""
+    title="SAM Quote Giver",
+    description="Get a real quote said by SAM himself.",
 )
+
+
+class Quote(BaseModel):
+    quote: str = Field(
+        description="The quote that SAM said.",
+    )
+    year: int = Field(
+        description="The year when SAM said the quote.",
+    )
+
+
+@app.get(
+    "/quote",
+    summary="Returns a random quote by SAM",
+    description="Upon receiving a GET request this endpoint will return a real quiote said by SAM himself.",
+    response_description="A Quote object that contains the quote said by SAM and the date when the quote was said.",
+    response_model=Quote,
+)
+def get_quote():
+    return {
+        "quote": "Life is short so eat it all.",
+        "year": 1950,
+    }
